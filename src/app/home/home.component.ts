e1a71882571fc1d4a601b47b61355c41d73351f0
Roman <neudakhroman@gmail.com>
2024-10-18 10:34:42 +0200

    init: initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import {HousingLocationComponent} from '../housing-location/housing-location.component';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  templateUrl:  './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
+}
